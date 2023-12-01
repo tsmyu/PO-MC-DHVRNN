94e37b2d7128675be18aa874608a39e8d612eedd
tsmyu <yuteshima18@gmail.com>
2023-12-01 09:57:45 +0900

    fix print

--- vrnn/datasets/__init__.py
+++ vrnn/datasets/__init__.py
@@ -3,10 +3,10 @@ from torch.utils.data import Dataset
 from importlib import import_module
 import cloudpickle
 
-class GeneralDataset(Dataset):
 
+class GeneralDataset(Dataset):
     def __init__(self, args, len_seqs, train=0, normalize_data=True):
-        #if normalize_data:
+        # if normalize_data:
         #    for index in range(len(activeRole)):
         #        data_x[index] = dataset.normalize(data[index],datasets)
 
@@ -19,36 +19,42 @@ class GeneralDataset(Dataset):
         self.model = args.model
         self.n_GorS = args.n_GorS
 
-        #self.normalize_data = normalize_data
-        #self.datasets = args.dataset
- 
+        # self.normalize_data = normalize_data
+        # self.datasets = args.dataset
+
     def __getitem__(self, index):
         if self.train == 1:
             batch_no = index // self.batchsize
             batch_index = index % self.batchsize
-            with open(self.game_files+'_tr'+str(batch_no)+'.pkl', 'rb') as f:
-                data,_,_,label = cloudpickle.load(f) # ,allow_pickle=True
+            with open(
+                self.game_files + "_tr" + str(batch_no) + ".pkl", "rb"
+            ) as f:
+                data, _, _, label = cloudpickle.load(f)  # ,allow_pickle=True
         else:
             J = 5 if self.n_GorS == 1 else 2
-            print(self.len_seqs)
-            batch_no = index // int(self.len_seqs/J)
+            batch_no = index // int(self.len_seqs / J)
             if batch_no < J:
-                batch_index = index % int(self.len_seqs/J)
+                batch_index = index % int(self.len_seqs / J)
             else:
-                batch_no = J-1
-                batch_index = index % int(self.len_seqs/J) + int(self.len_seqs/J)
-            filename = self.game_files+'_val' if self.train == 0 else self.game_files +'_te'
-            with open(filename+'_'+str(batch_no)+'.pkl', 'rb') as f:
-                data,label = cloudpickle.load(f) 
+                batch_no = J - 1
+                batch_index = index % int(self.len_seqs / J) + int(
+                    self.len_seqs / J
+                )
+            filename = (
+                self.game_files + "_val"
+                if self.train == 0
+                else self.game_files + "_te"
+            )
+            with open(filename + "_" + str(batch_no) + ".pkl", "rb") as f:
+                data, label = cloudpickle.load(f)
 
         self.data = torch.Tensor(data)
         self.data = self.data.permute(1, 0, 2, 3)
 
-        #if self.normalize_data:
+        # if self.normalize_data:
         #    self.data = self.dataset.normalize(self.data,self.datasets)
 
-        return self.data[batch_index], label[batch_index] # data[index] 
+        return self.data[batch_index], label[batch_index]  # data[index]
 
     def __len__(self):
         return self.len_seqs
-        

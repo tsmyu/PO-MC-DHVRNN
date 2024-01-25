f3d70428fe4c9f9b6abebb725c57f2a84dbff2d6
tsmyu <yuteshima18@gmail.com>
2024-01-25 11:08:15 +0900

    modify for val results

--- make_results_fig/result_topview.py
+++ make_results_fig/result_topview.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 import os
 import glob
@@ -8,24 +9,31 @@ import json
 from natsort import natsorted
 from matplotlib.backends.backend_pdf import PdfPages
 
+sns.set(
+    "paper",
+    "whitegrid",
+    "bright",
+    font_scale=2.5,
+    rc={"lines.linewidth": 3, "grid.linestyle": "--"},
+)
+
 
 with open(
-    "./weights/sigmoid_BCE/epoch200_aoki/params.p",
+    "./weights/for_paper/kiku/params.p",
     "rb",
 ) as f:
     param = np.load(f, allow_pickle=True)
     # print(param)
     predict_time = param["burn_in"]
-
 with open(
-    "./weights/sigmoid_BCE/epoch200_aoki/experiments/sample/samples.p",
+    "./weights/for_paper/kiku/samples_val.p",
     "rb",
 ) as f:
     data = np.load(f, allow_pickle=True)
 
 # import pdb; pdb.set_trace()
-os.makedirs("./teshima/result/pulse", exist_ok=True)
-pp = PdfPages("./teshima/result/pulse/topview_vrnn_kiku.pdf")
+os.makedirs("./weights/for_paper/kiku/results", exist_ok=True)
+pp = PdfPages("./weights/for_paper/kiku/results/topview_vrnn_kiku_val.pdf")
 
 count = 0
 
@@ -104,11 +112,8 @@ for episode in range(len(data[0][0][0][0])):
                 + (test_vel_x - train_vel_x) ** 2
             )
         )
-
     # print(test_pulse)
     # print(train_pulse.count(0))
-
-    # import pdb; pdb.set_trace()
 
     fig = plt.figure(figsize=(6, 6))
     ax = fig.add_subplot(111)
@@ -136,85 +141,59 @@ for episode in range(len(data[0][0][0][0])):
     obs_x = obs_point_dict[Env_name]["x"]
     obs_y = obs_point_dict[Env_name]["y"]
 
-    ax.scatter(
-        obs_x,
-        obs_y,
-        marker="o",
-        label="chain",
-        color="#ff7f0e",
+    sns.scatterplot(
+        x=obs_x,
+        y=obs_y,
+        marker="s",
+        # label="chain",
+        color="green",
         s=8,
-        zorder=1,
     )
 
-    #############12s###############
-    # if count <= 23:
-    #     from obstacle_information.EnvRegular_20230421 import obs_x, obs_y
-    #     ax.scatter(obs_x, obs_y, marker='o', label='chain', color='#ff7f0e', s=8)
-    # elif count >= 24 and count <= 37:
-    #     from obstacle_information.EnvRegular_20221018 import obs_x, obs_y
-    #     ax.scatter(obs_x, obs_y, marker='o', label='chain', color='#ff7f0e', s=8)
-    # elif count >= 38 and count <= 51:
-    #     from obstacle_information.EnvRegular_20230429 import obs_x, obs_y
-    #     ax.scatter(obs_x, obs_y, marker='o', label='chain', color='#ff7f0e', s=8)
-    # elif count >= 52 and count <= 74:
-    #     from obstacle_information.EnvRegular_20230428 import obs_x, obs_y
-    #     ax.scatter(obs_x, obs_y, marker='o', label='chain', color='#ff7f0e', s=8)
-    # elif count >= 75 and count <= 80:
-    #     from obstacle_information.EnvRegular_20221011 import obs_x, obs_y
-    #     ax.scatter(obs_x, obs_y, marker='o', label='chain', color='#ff7f0e', s=8)
-    # elif count >= 81 and count <= 105:
-    #     from obstacle_information.EnvRegular_20221003 import obs_x, obs_y
-    #     ax.scatter(obs_x, obs_y, marker='o', label='chain', color='#ff7f0e', s=8)
-
-    # ax.plot(train_x, train_y, label='mesured flight path', color='#1f77b4')
-    ax.plot(
-        train_x_pre,
-        train_y_pre,
-        label="measured flight path",
-        color="#1f77b4",
+    sns.lineplot(
+        x=train_x_pre,
+        y=train_y_pre,
+        # label="measured",
+        sort=False,
         linestyle="--",
-        zorder=2,
-        linewidth=3,
     )
-    ax.plot(
-        test_x,
-        test_y,
-        label="predicted flight path",
-        color="#d62728",
-        zorder=2,
-        linewidth=3,
+    sns.lineplot(
+        x=test_x,
+        y=test_y,
+        # label="predicted",
+        sort=False,
     )
 
-    for i in range(len(train_pulse)):
-        if train_pulse[i] >= 0.5:
-            ax.scatter(
-                train_x_pre[i],
-                train_y_pre[i],
-                label="measured pulse timing",
-                color="w",
-                edgecolors="#1f77b4",
-                s=20,
-                zorder=3,
-            )
-        if test_pulse[i] >= 0.5:
-            ax.scatter(
-                test_x[i],
-                test_y[i],
-                label="predicted pulse timing",
-                color="w",
-                edgecolors="#d62728",
-                s=20,
-                zorder=3,
-            )
+    # for i in range(len(train_pulse)):
+    #     if train_pulse[i] >= 0.5:
+    #         ax.scatter(
+    #             train_x_pre[i],
+    #             train_y_pre[i],
+    #             label="measured pulse timing",
+    #             color="w",
+    #             edgecolors="#1f77b4",
+    #             s=20,
+    #             zorder=3,
+    #         )
+    #     if test_pulse[i] >= 0.5:
+    #         ax.scatter(
+    #             test_x[i],
+    #             test_y[i],
+    #             label="predicted pulse timing",
+    #             color="w",
+    #             edgecolors="#d62728",
+    #             s=20,
+    #             zorder=3,
+    #         )
 
     plt.xlabel("X [m]")
     plt.ylabel("Y [m]")
 
-    plt.xticks([0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5])
-    plt.yticks([0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5])
+    plt.xticks([0, 1.5, 3.0, 4.5])
+    plt.yticks([0, 1.5, 3.0, 4.5])
     plt.tight_layout()
     ax.set_aspect("equal")
-    # plt.legend(bbox_to_anchor=(1,1), loc='upper left')
+    # plt.legend(bbox_to_anchor=(1, 1), ncol=1, fontsize=10)
 
     pp.savefig(fig)
 
